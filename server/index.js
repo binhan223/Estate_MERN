1ba2bc0772b6a2a5eb940479871683aabaa5b66d
binhan <tranbinhan223@gmail.com>
2024-04-12 20:09:18 +0700

    create signup signin api routes

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_DB).then(() => {
     console.log('Connected to MongoDB');
 }).catch ((err) => {
     console.log(err);
+    process.exit(1);
 });
 
 
@@ -17,10 +18,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cookieParser);
+// app.use(cookieParser);
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
