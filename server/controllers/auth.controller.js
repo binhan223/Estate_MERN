80ed5a990653174f87035aa1738dbc9999f4a52a
binhan <tranbinhan223@gmail.com>
2024-04-12 20:15:49 +0700

    handle possible error

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,39 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
+
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const existUser = await User.findOne({ email });
+    if(existUser) return next(errorHandler(404, 'User exist'));
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    try {
+        await newUser.save();
+        res.status(201).json('User created');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const existUser = await User.findOne({ email });
+        if(!existUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compare(password, existUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+        const token = jwt.sign({ id: existUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = existUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    } 
+};
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,19 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
+
+export const getUser = async (req, res, next) => {
+    try {
+        
+        const user = await User.findById(req.params.id);
+
+        if (!user) return next(errorHandler(404, 'User not found'));
+        
+        const {password: pass, ...rest} = user._doc;
+
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
