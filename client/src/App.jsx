2b8180f6770c1efb07906587ad37923f597e6be7
binhan <tranbinhan223@gmail.com>
2024-04-14 20:22:08 +0700

    update header and create profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,9 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 // import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
-import Search from './pages/Search';
 import Header from './components/Header';
+import Profile from './pages/Profile';
+import PrivateRoute from './components/PrivateRoute';
 
 
 export default function App() {
@@ -15,8 +16,9 @@ export default function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/search' element={<Search/>}/>
-
+        <Route element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
