31e0c98c114bdd59595ac8cff7aabfbcd6f01469
binhan <tranbinhan223@gmail.com>
2024-04-14 19:43:43 +0700

    add google auth functionally

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPVU6aTvTGigbTjI-1cRpWJ-k1M4ADSSs",
+  authDomain: "estate-mern-98283.firebaseapp.com",
+  projectId: "estate-mern-98283",
+  storageBucket: "estate-mern-98283.appspot.com",
+  messagingSenderId: "326078076331",
+  appId: "1:326078076331:web:4a2f118ceecc48d4f5aeee"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -73,6 +74,7 @@ export default function SignUp() {
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading...' : 'Đăng Ký'}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p> Đã có tài khoản?</p>
